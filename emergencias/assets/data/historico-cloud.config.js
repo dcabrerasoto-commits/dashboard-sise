149739d2965212e01c7ca2acaf0cf9a80d8f4a45
dcabrerasoto-commits <dcabrerasoto@gmail.com>
2026-07-17 16:55:51 -0400

    Activar opción sin información regional

--- emergencias/assets/data/historico-cloud.config.js
+++ emergencias/assets/data/historico-cloud.config.js
@@ -1,2 +1,10 @@
-// Pegue aqui la URL del Web App de Apps Script para consolidar el ingreso regional.
+// URL del Web App de Apps Script para consolidar el ingreso regional.
 window.UISE_HISTORICO_ENDPOINT = "https://script.google.com/macros/s/AKfycbx5EoFvhisRV1MbtOdVwa_OJz-V1oc0iIeO4yz-GlMG45HeL3wKDDE3Pi-W65Bj9bIm/exec";
+
+// Mejora del formulario: permite informar que la región no tiene antecedentes nuevos.
+(function cargarEstadoSinInformacion(){
+  const script=document.createElement('script');
+  script.src='assets/js/no-info-report.js?v=20260717';
+  script.async=false;
+  document.head.appendChild(script);
+})();
