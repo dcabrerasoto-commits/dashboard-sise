7c22407d946c64e49f94c66ab081b534b319b086
dcabrerasoto-commits <dcabrerasoto@gmail.com>
2026-07-17 12:24:08 -0400

    Configurar endpoint central de ingreso regional

--- emergencias/assets/data/historico-cloud.config.js
+++ emergencias/assets/data/historico-cloud.config.js
@@ -1,2 +1,2 @@
 // Pegue aqui la URL del Web App de Apps Script para consolidar el ingreso regional.
-window.UISE_HISTORICO_ENDPOINT = "";
+window.UISE_HISTORICO_ENDPOINT = "https://script.google.com/macros/s/AKfycbx5EoFvhisRV1MbtOdVwa_OJz-V1oc0iIeO4yz-GlMG45HeL3wKDDE3Pi-W65Bj9bIm/exec";
