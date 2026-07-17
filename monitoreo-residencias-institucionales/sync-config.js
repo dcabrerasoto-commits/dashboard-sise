ff971f7f1c69ddd2244c7ecddbc5554c9c1efc17
dcabrerasoto-commits <dcabrerasoto@gmail.com>
2026-07-17 12:01:21 -0400

    Configurar sincronizacion residencias

--- monitoreo-residencias-institucionales/sync-config.js
+++ monitoreo-residencias-institucionales/sync-config.js
@@ -1,4 +1,4 @@
 window.MONITOREO_SYNC_CONFIG = {
-  webAppUrl: "",
+  webAppUrl: "https://script.google.com/macros/s/AKfycbwvfpzSOf0mucfMELdIA6gIdPSTE8K0eYlY3bU8wG1KpMSxDznBTiwThR6_HEkHkzpa/exec",
   spreadsheetUrl: "https://docs.google.com/spreadsheets/d/1-j0ItNOgWTvYjWlmnks1JCelQ-zhXN4Gii_QVq05ByA/edit?usp=sharing"
 };
